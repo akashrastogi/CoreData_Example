f168f1c6f25c9459369910f1bd4a95d1944708f9
akashrastogi <akash_rastogi@live.com>
2016-10-22 22:20:34 +0530

    Initial Commit

---
+++ CoreData_Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData_Example
+//
+//  Created by HealthOne on 22/10/16.
+//  Copyright © 2016 akash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData_Example/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreData_Example
+//
+//  Created by HealthOne on 22/10/16.
+//  Copyright © 2016 akash. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
